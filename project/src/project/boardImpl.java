856c00242420e550793fa0dd6fc50fe71a93d81a
user <a@a.com>
2023-06-29 13:58:09 +0900

    5145

--- project/src/project/boardImpl.java
+++ project/src/project/boardImpl.java
@@ -10,20 +10,20 @@ public class boardImpl implements boardService{
 
 	@Override
 	public void display() {
-
-
-	}
-
-	@Override
-	public void posting() {
 		System.out.println("===작성된 글 목록===");
 		System.out.println("제목\t작성자\t글 내용\t날짜");
 		System.out.println("------------------------");
 		for(int i =0; i<List.size(); i++) {
 			boardDTO dto = List.get(i);
 			System.out.println(dto.getTitle()+"\t"+dto.getWriter()+"\t"+dto.getContents()+"\t"+dto.getPostingDate());
-			System.out.println("------------------------------");
+			System.out.println("-----------------------------------");
 		}
+
+	}
+
+	@Override
+	public void posting() {
+	
 			
 
 	}
