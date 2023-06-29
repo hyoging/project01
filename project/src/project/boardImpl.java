c14b8edd918923f19223ef3ce3dcadfacc774347
user <a@a.com>
2023-06-29 13:54:25 +0900

    123

--- project/src/project/boardImpl.java
+++ project/src/project/boardImpl.java
@@ -1,17 +1,31 @@
 package project;
 
+import java.util.ArrayList;
+
 public class boardImpl implements boardService{
+	private ArrayList<boardDTO> List;
+	public boardImpl() {
+		List = new ArrayList<>();
+	}
 
 	@Override
 	public void display() {
-		
-		
+
+
 	}
 
 	@Override
 	public void posting() {
-		
-		
+		System.out.println("===작성된 글 목록===");
+		System.out.println("제목\t작성자\t글 내용\t날짜");
+		System.out.println("------------------------");
+		for(int i =0; i<List.size(); i++) {
+			boardDTO dto = List.get(i);
+			System.out.println(dto.getTitle()+"\t"+dto.getWriter()+"\t"+dto.getContents()+"\t"+dto.getPostingDate());
+			System.out.println("------------------------------");
+		}
+			
+
 	}
 
 }
